fb2418c26c043780f6e0b8719c408845aa3549d7
Samidíase <samilly.s22s@gmail.com>
2024-09-06 13:50:03 -0300

    primeiro commeit

---
+++ app.js
@@ -0,0 +1,17 @@
+// console.log(dados);
+
+let section = document.getElementById("resultados-pesquisa")
+console.log(section);
+
+section.innerHTML = `
+        <div class="item-resultado">
+            <h2> 
+                <a href="#" target="_blank">Rebeca Andrade</a>
+            </h2>
+            <p class="descrição-meta">
+                Rebeca Andrade é um nome que brilha no cenário da ginástica mundial. Com talento, determinação e uma força de vontade inigualável, a brasileira conquistou o coração dos fãs e se tornou um verdadeiro símbolo do esporte nacional 
+            </p>
+            <a href="https://pt.wikipedia.org/wiki/Rebeca_Andrade" target="_blank">Mais 
+            informações</a>
+        </div>
+        `;
---
+++ dados.js
@@ -0,0 +1,12 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+    descriçao : "Rayssa Leal,é uma skatista brasileira que, com apenas 13 anos, conquistou o mundo com seu talento e carisma.",
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo : "Rebeca Andrade",
+    descriçao : "Rebeca Andrade é um nome que brilha no cenário da ginástica mundial. Com talento, determinação e uma força de vontade inigualável, a brasileira conquistou o coração dos fãs e se tornou um verdadeiro símbolo do esporte nacional",
+    link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade" 
+    },
+]
